3d19e2c00157f1edd89a4655ff24b8b345ddcac7
GuillaumeRoPu <gllmrllndptz@gmail.com>
2025-05-21 23:15:27 +0200

    Added Combo Scaling up and down

--- Assets/Scripts/Ui/ComboMultiplicator.cs
+++ Assets/Scripts/Ui/ComboMultiplicator.cs
@@ -7,6 +7,7 @@ public class ComboMultiplicator : MonoBehaviour
     private bool _increase;
     private Vector3 _originalScale;
     private float _timer;
+    private bool _reverse;
 
     private void Start()
     {
@@ -19,18 +20,56 @@ public class ComboMultiplicator : MonoBehaviour
     {
         if (_canAnimate && _increase)
         {
-            _timer += Time.deltaTime;
-            gameObject.transform.localScale = Mathf.Lerp(_originalScale, _originalScale*2, );
+            if (!_reverse)
+            {
+                _timer += Time.deltaTime;
+                _timer = Mathf.Clamp(_timer, 0f, _animationSpeed);
+                gameObject.transform.localScale = new Vector3(Mathf.Lerp(_originalScale.x, _originalScale.x * 2, _timer / _animationSpeed), Mathf.Lerp(_originalScale.y, _originalScale.y * 2, _timer / _animationSpeed), Mathf.Lerp(_originalScale.z, _originalScale.z * 2, _timer / _animationSpeed));
+            }
+            else
+            {
+                _timer -= Time.deltaTime;
+                _timer = Mathf.Clamp(_timer, 0f, _animationSpeed);
+                gameObject.transform.localScale = new Vector3(Mathf.Lerp(_originalScale.x, _originalScale.x * 2, _timer / _animationSpeed), Mathf.Lerp(_originalScale.y, _originalScale.y * 2, _timer / _animationSpeed), Mathf.Lerp(_originalScale.z, _originalScale.z * 2, _timer / _animationSpeed));
+            }
+            if (_timer >= _animationSpeed)
+                _reverse = true;
+            if (_timer == 0f && _reverse)
+            {
+                _reverse = false;
+                _canAnimate = false;
+            }
         }
         else if (_canAnimate && !_increase)
         {
-
+            if (!_reverse)
+            {
+                _timer += Time.deltaTime;
+                _timer = Mathf.Clamp(_timer, 0f, _animationSpeed);
+                gameObject.transform.localScale = new Vector3(Mathf.Lerp(_originalScale.x, _originalScale.x * 0.5f, _timer / _animationSpeed), Mathf.Lerp(_originalScale.y, _originalScale.y * 0.5f, _timer / _animationSpeed), Mathf.Lerp(_originalScale.z, _originalScale.z * 0.5f, _timer / _animationSpeed));
+            }
+            else
+            {
+                _timer -= Time.deltaTime;
+                _timer = Mathf.Clamp(_timer, 0f, _animationSpeed);
+                gameObject.transform.localScale = new Vector3(Mathf.Lerp(_originalScale.x, _originalScale.x * 0.5f, _timer / _animationSpeed), Mathf.Lerp(_originalScale.y, _originalScale.y * 0.5f, _timer / _animationSpeed), Mathf.Lerp(_originalScale.z, _originalScale.z * 0.5f, _timer / _animationSpeed));
+            }
+            if (_timer >= _animationSpeed)
+                _reverse = true;
+            if (_timer == 0f && _reverse)
+            {
+                _reverse = false;
+                _canAnimate = false;
+            }
         }
     }
 
     public void CoolAnimation(bool increase)
     {
+        Debug.Log("Sent Message");
+        _timer = 0f;
         _increase = increase;
         _canAnimate = true;
+        _reverse = false;
     }
 }
